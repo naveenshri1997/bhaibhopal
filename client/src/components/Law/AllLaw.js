09064170c50073c05e82d74ab3b3463298b1f7b9
naveenshri1997 <101866892+naveenshri1997@users.noreply.github.com>
2023-08-29 18:50:45 +0530

    Update AllLaw.js

--- client/src/components/Law/AllLaw.js
+++ client/src/components/Law/AllLaw.js
@@ -4,7 +4,7 @@ import IpcLaw from './IpcLaw';
 const AllLaw = () => {
     const [law, setLaw] = useState([]);
     useEffect(() => {
-        const res = fetch('https://server-dccr.onrender.com/lawdatanavbar', {
+        const res = fetch('https://bhaibhopal.onrender.com/lawdatanavbar', {
             method: 'GET',
         }).then((res) => res.json()).then((data) => {
             setLaw(data.data);
